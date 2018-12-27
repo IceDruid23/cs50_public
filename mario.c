5080246ab571660dc0cd9b57c054b60325251013
IceDruid23 <IceDruid23@users.noreply.github.com>
2018-12-27 01:36:23 +0000

    20181227T013601Z

---
+++ mario.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+//program to draw a half pyramid
+int main(void)
+{
+    int n = -1; //max number of blocks
+    char m[20]; // used for integer corectness only
+
+
+    //prompt user for input
+    do
+    {
+        printf("Height: ");
+        scanf("%s", m); // read number as string
+        if (strncmp(m, "0", 1) == 0) // exit if number 0
+        {
+            break;
+        }
+        n = atoi(m); // convert to integer
+
+    }
+    while (n <= 0 || n > 23);
+
+    //building the pyramid
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = n + 1; j > 0; j--) //first half of pyramid
+        {
+            j > i + 1 ? printf(" ") : printf("#");
+        }
+
+        printf("  ");
+
+        for (int j = 0; j < n+1; j++) //second half of pyramid
+        {
+            j < i + 1 ? printf("#") : printf(" ");
+        }
+
+        printf("\n");
+    }
+}
