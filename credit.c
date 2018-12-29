0aec4cd9e71fd546c6eb7e54964bd7478915f114
IceDruid23 <IceDruid23@users.noreply.github.com>
2018-12-29 00:28:12 +0000

    20181229T002801Z

--- credit.c
+++ credit.c
@@ -79,7 +79,14 @@ int main(void)
 
     }
 }
-//the checksum calculus, valid if last digit of the checksum is 0
+//CHECKSUM CALCULUS
+/*
+Multiply every other digit by 2, starting with the number’s second-to-last digit, and then add those products' digits together.
+
+Add the sum to the sum of the digits that weren’t multiplied by 2.
+
+If the total’s last digit is 0 (or, put more formally, if the total modulo 10 is congruent to 0), the number is valid!
+*/
 void sum_check(int long long n, char card_type[10])
 {
     int x, sum = 0;
